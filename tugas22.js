9a9699ea72454b14bc8afe1c15f2ccde5af1ab54
rizki wahyudie git <rizkiwahyudie@gmail.com>
2022-02-05 22:07:25 +0700

    add file

---
+++ tugas22.js
@@ -0,0 +1,7 @@
+function forEach(){
+  var x = ["Saya","ingin","belajar","bersama"];
+  x.forEach(function(item, index, array) {
+    console.log("item:",item,"index ke",index);
+  });
+}
+forEach();
